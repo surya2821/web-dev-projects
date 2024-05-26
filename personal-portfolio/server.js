531f5aa9bfe9f623e626bd72b2b9f0c800b6c48d
surya2821 <allasaisurya124@gmail.com>
2024-05-26 09:47:21 +0530

    changed server.js

--- personal-portfolio/server.js
+++ personal-portfolio/server.js
@@ -3,23 +3,29 @@ const router = express.Router();
 const cors = require("cors");
 const nodemailer = require("nodemailer");
 
-// server used to send send emails
+// Load environment variables
+require('dotenv').config();
+
+// Create Express app
 const app = express();
 app.use(cors());
 app.use(express.json());
 app.use("/", router);
-app.listen(5000, () => console.log("Server Running"));
-console.log(process.env.EMAIL_USER);
-console.log(process.env.EMAIL_PASS);
 
+// Start the server
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server Running on port ${PORT}`));
+
+// Create nodemailer transport
 const contactEmail = nodemailer.createTransport({
   service: 'gmail',
   auth: {
-    user: "********@gmail.com",
-    pass: ""
+    user: process.env.EMAIL_USER, // Use environment variable for email user
+    pass: process.env.EMAIL_PASS // Use environment variable for email password
   },
 });
 
+// Verify email transport
 contactEmail.verify((error) => {
   if (error) {
     console.log(error);
@@ -28,6 +34,7 @@ contactEmail.verify((error) => {
   }
 });
 
+// Handle POST request to /contact endpoint
 router.post("/contact", (req, res) => {
   const name = req.body.firstName + req.body.lastName;
   const email = req.body.email;
@@ -35,13 +42,15 @@ router.post("/contact", (req, res) => {
   const phone = req.body.phone;
   const mail = {
     from: name,
-    to: "********@gmail.com",
+    to: process.env.EMAIL_USER, // Use environment variable for recipient email
     subject: "Contact Form Submission - Portfolio",
     html: `<p>Name: ${name}</p>
            <p>Email: ${email}</p>
            <p>Phone: ${phone}</p>
            <p>Message: ${message}</p>`,
   };
+  
+  // Send email
   contactEmail.sendMail(mail, (error) => {
     if (error) {
       res.json(error);
