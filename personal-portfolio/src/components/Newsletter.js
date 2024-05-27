cb5c8b1ed441abcadd85c8ead6a83a50a08c2c3e
surya2821 <allasaisurya124@gmail.com>
2024-05-27 22:59:13 +0530

    changed the mail button

--- personal-portfolio/src/components/Newsletter.js
+++ personal-portfolio/src/components/Newsletter.js
@@ -21,26 +21,5 @@ export const Newsletter = ({ status, message, onValidated }) => {
     setEmail('');
   }
 
-  return (
-      <Col lg={12}>
-        <div className="newsletter-bx wow slideInUp">
-          <Row>
-            <Col lg={12} md={6} xl={5}>
-              <h3>Phone Number & Gmail---8096551517<br></br>allasaisurya124@gmail.com</h3>
-              {status === 'sending' && <Alert>Sending...</Alert>}
-              {status === 'error' && <Alert variant="danger">{message}</Alert>}
-              {status === 'success' && <Alert variant="success">{message}</Alert>}
-            </Col>
-            <Col md={6} xl={7}>
-              <form onSubmit={handleSubmit}>
-                <div className="new-email-bx">
-                  <input value={email} type="email" onChange={(e) => setEmail(e.target.value)} placeholder="Email Address" />
-                  <button type="submit">Submit</button>
-                </div>
-              </form>
-            </Col>
-          </Row>
-        </div>
-      </Col>
-  )
+ 
 }
