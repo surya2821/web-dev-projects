dd1100587fda7bb2ac4166c16090174fca50347b
surya2821 <allasaisurya124@gmail.com>
2024-11-19 12:22:48 +0530

    changeddiscordicon

--- personal-portfolio/src/components/NavBar.js
+++ personal-portfolio/src/components/NavBar.js
@@ -4,6 +4,7 @@ import { Navbar, Nav, Container } from "react-bootstrap";
 import navIcon1 from '../assets/img/nav-icon1.svg';
 import navIcon2 from '../assets/img/nav-icon2.svg';
 import navIcon3 from '../assets/img/nav-icon3.svg';
+import navIcon4 from '../assets/img/nav-icon4.svg';
 import { HashLink } from 'react-router-hash-link';
 import {
   BrowserRouter as Router
@@ -49,12 +50,19 @@ export const NavBar = () => {
             </Nav>
             </div>
             </center>
+            <widgetbot server="299881420891881473"
+            channel="355719584830980096"
+            width="800"
+             height="600"
+             ></widgetbot>
+<script src="https://cdn.jsdelivr.net/npm/@widgetbot/html-embed"></script>
             
             <span className="navbar-text">
               <div className="social-icon">
                 <a href="https://www.linkedin.com/in/saisurya2818/"><img src={navIcon1} alt="" /></a>
                 <a href="https://github.com/surya2821"><img src={navIcon2} alt="" /></a>
                 <a href="https://www.instagram.com/surya_2820_/"><img src={navIcon3} alt="" /></a>
+                <a href="https://discord.gg/K2W6DqUR"><img src={navIcon4} alt="" /></a>
               </div>
               
            
